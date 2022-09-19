212b9e75ee631222ee1d408c7fb0cb316bdc5fc2
Mark LeMoine <linclelinkpart5@gmail.com>
2022-09-19 12:10:59 -0700

    Port over I2C basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::{I2c, Peripherals};
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
+    let dp = Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    // let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    let mut _i2c = I2c::new(
+        dp.TWI,
+        // `A4`: I2C SDA signal.
+        pins.a4.into_pull_up_input(),
+        // `A5`: I2C SCL signal.
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+
     loop {}
 }
